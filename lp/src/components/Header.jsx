fbd185fb5fa9db14dbf073228a800e6a0b4b0a28
Александр <shalser82@gmail.com>
2022-08-03 05:59:21 +0300

    add code

---
+++ lp/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  max-width: 1920px;
+  height: 70px;
+`;
+
+const Title = styled.h1`
+  font-size: 25px;
+`;
+
+const Header = () => {
+    return (
+        <HeaderWrapper>
+            <Title>Learn React</Title>
+            <HeaderWrapper/>
+            );
+            };
+
+            export default Header;
+
