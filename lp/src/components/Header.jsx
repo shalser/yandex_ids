8184cd7c8d9200054a5329282efc1a9c13e647e1
shalser <shalser82@gmail.com>
2022-08-21 10:27:13 +0300

    trying to do something

--- lp/src/components/Header.jsx
+++ lp/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 
+
 const HeaderWrapper = styled.div`
   width: 100%;
   max-width: 1920px;
@@ -11,10 +12,26 @@ const Title = styled.h1`
   font-size: 25px;
 `;
 
+const Wrap = styled.h1`
+  font-size: 25px;
+`;
+
 const Header = () => {
+    function handleClick() {
+        let div = document.querySelector('.div');
+        div.classList.toggle('active');
+
+        if (div.classList.contains('active')) {
+            div.innerHTML = '<h1>Hello World</h1>';
+        } else {
+            div.innerHTML = '<h1>Hello</h1>';
+        }
+    }
+
     return (
         <HeaderWrapper>
             <Title>Learn React</Title>
+            <Wrap className='div' onClick={handleClick}><h1>Hello</h1></Wrap>
         </HeaderWrapper>
     );
 };
